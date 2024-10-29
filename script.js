2568ec58d4c26a12401d110d59852a370809775d
KateCheng26 <112001487+KateCheng26@users.noreply.github.com>
2024-10-29 11:53:59 -0400

    .

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const printPage = () => {
-    const printFrame = document.createElement('iframe');
-    printFrame.style.display = 'none';
-    printFrame.src = 'index.html';
-    document.body.appendChild(printFrame);
-    printFrame.contentWindow.focus();
-    printFrame.contentWindow.print();
-};
+// const printPage = () => {
+//     const printFrame = document.createElement('iframe');
+//     printFrame.style.display = 'none';
+//     printFrame.src = 'index.html';
+//     document.body.appendChild(printFrame);
+//     printFrame.contentWindow.focus();
+//     printFrame.contentWindow.print();
+// };
