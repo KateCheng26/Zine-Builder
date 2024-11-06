e4eada8b085d017f3d6c383d64364b231624ca9c
KateCheng26 <112001487+KateCheng26@users.noreply.github.com>
2024-11-06 14:11:44 -0500

    Delete go away directory

--- go away/script.js
+++
@@ -1,8 +0,0 @@
-// const printPage = () => {
-//     const printFrame = document.createElement('iframe');
-//     printFrame.style.display = 'none';
-//     printFrame.src = 'index.html';
-//     document.body.appendChild(printFrame);
-//     printFrame.contentWindow.focus();
-//     printFrame.contentWindow.print();
-// };
