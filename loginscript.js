56e2f4488979c39a6208e200462d6d84bb6dc221
KateCheng26 <112001487+KateCheng26@users.noreply.github.com>
2024-11-08 09:04:07 -0500

    Create loginscript.js

---
+++ loginscript.js
@@ -0,0 +1,35 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, query, where } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDaacl1g-63bYmdKk0EzPHMLe5CZLU53Sk",
+    authDomain: "zine-builder.firebaseapp.com",
+    projectId: "zine-builder",
+    storageBucket: "zine-builder.appspot.com",
+    messagingSenderId: "1037717512420",
+    appId: "1:1037717512420:web:51b5d77bec663069310cf1"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+//login function for submit button
+export const login = function (email, password){
+  //call signInWithEmailAndPassword, firebase function
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    //sucessful sign-in: update window
+    window.location.href = "mockup.html";
+  })
+  .catch((error) => {
+    //error catch log messages
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
